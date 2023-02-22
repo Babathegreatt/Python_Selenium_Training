d2e7c560e16514691741590f8d8559303a43ed2e
Gaurav Inamdar <Gaurav.Inamdar@einfochips.com>
2023-02-22 11:50:25 +0530

    Third Commit

---
+++ demo8_loops/demo1.py
@@ -0,0 +1,15 @@
+# print 1 to 10
+
+for a in range(1, 6, 2):
+    print(a)
+
+colors = ["red", "green", "yellow", "blue", "green"]
+
+for i in range(0, len(colors)):
+    print(colors[i])
+
+numbers = [45, 98, 75, 65, 24, 88, 74, 56, 75]
+
+for i in range(0, len(numbers)):
+    if numbers[i] <= 50:
+        print(numbers[i])
